7077381de4d5577164b30c4ce035533b72271fcb
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-01-05 23:14:25 +0100

    added block

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-let formElement = document.querySelector(".js-form");
-let amountValueElement = document.querySelector(".js-amountValue");
-let selectElement = document.querySelector(".js-select");
-let finishTextElement = document.querySelector(".js-finishText")
-let usd = 4.41;
-let eur = 4.69;
-let gbp = 5.30;
+{
+    let formElement = document.querySelector(".js-form");
+    let amountValueElement = document.querySelector(".js-amountValue");
+    let selectElement = document.querySelector(".js-select");
+    let finishTextElement = document.querySelector(".js-finishText")
+    let usd = 4.41;
+    let eur = 4.69;
+    let gbp = 5.30;
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let amount = amountValueElement.value
-    switch (selectElement.value) {
-        case "USD":
-            result = amount / usd;
-            break;
-        case "EUR":
-            result = amount / eur;
-            break;
-        case "GBP":
-            result = amount / gbp;
-            break;
-    }
-    finishTextElement.innerText = `Za ${amountValueElement.value}ZŁ otrzymamy ${result.toFixed(2)} ${selectElement.value}`
-})
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
+        let amount = amountValueElement.value
+        switch (selectElement.value) {
+            case "USD":
+                result = amount / usd;
+                break;
+            case "EUR":
+                result = amount / eur;
+                break;
+            case "GBP":
+                result = amount / gbp;
+                break;
+        }
+        finishTextElement.innerText = `Za ${amountValueElement.value}ZŁ otrzymamy ${result.toFixed(2)} ${selectElement.value}`
+    })
+}
