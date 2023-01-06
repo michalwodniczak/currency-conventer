63939055815e35fd07bf24fa6a467d7c456f8285
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-01-06 13:54:04 +0100

    added function calculateResult

--- script.js
+++ script.js
@@ -3,24 +3,29 @@
     const amountValueElement = document.querySelector(".js-amountValue");
     const selectElement = document.querySelector(".js-select");
     const finishTextElement = document.querySelector(".js-finishText")
-    const usd = 4.41;
-    const eur = 4.69;
-    const gbp = 5.30;
+
+
+    const calculateResult = (amount,selectElement) => {
+        const usd = 4.41;
+        const eur = 4.69;
+        const gbp = 5.30;
+
+        switch (selectElement.value) {
+            case "USD":
+                return amount / usd;
+
+            case "EUR":
+                return amount / eur;
+
+            case "GBP":
+                return amount / gbp;
+        }
+    }
 
     formElement.addEventListener("submit", (event) => {
         event.preventDefault();
         const amount = amountValueElement.value
-        switch (selectElement.value) {
-            case "USD":
-                result = amount / usd;
-                break;
-            case "EUR":
-                result = amount / eur;
-                break;
-            case "GBP":
-                result = amount / gbp;
-                break;
-        }
+        const result = calculateResult(amount,selectElement)
         finishTextElement.innerText = `Za ${amountValueElement.value}ZŁ otrzymamy ${result.toFixed(2)} ${selectElement.value}`
     })
 }
