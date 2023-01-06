38f525673c8e0019e2d3bc137d903275f4e761b9
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-01-06 14:01:05 +0100

    added function onResultText

--- script.js
+++ script.js
@@ -15,18 +15,22 @@
                 return amount / gbp;
         }
     }
+
+    const onResultText = (amountValueElement, selectElement, result) => {
+        const finishTextElement = document.querySelector(".js-finishText")
+        finishTextElement.innerText = `Za ${amountValueElement.value}ZŁ otrzymamy ${result.toFixed(2)} ${selectElement.value}`
+    }
+
     const init = () => {
         const formElement = document.querySelector(".js-form");
         const amountValueElement = document.querySelector(".js-amountValue");
         const selectElement = document.querySelector(".js-select");
-        const finishTextElement = document.querySelector(".js-finishText")
         const amount = amountValueElement.value
-
 
         formElement.addEventListener("submit", (event) => {
             event.preventDefault();
             const result = calculateResult(amount, selectElement)
-            finishTextElement.innerText = `Za ${amountValueElement.value}ZŁ otrzymamy ${result.toFixed(2)} ${selectElement.value}`
+            onResultText(amountValueElement, selectElement, result)
         })
     }
     init();
